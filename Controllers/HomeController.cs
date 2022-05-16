7d1789a5fc9abaf36e06739143ce5c4cfe0d7537
Diaksemor <32800211+Shimory@users.noreply.github.com>
2022-05-16 21:16:43 +0300

    First commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,114 @@
+﻿using Adresbook.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Adresbook.Controllers
+
+{
+ 
+    public class HomeController : Controller
+    {
+        UserContext db;
+        public HomeController(UserContext context)
+        {
+            db = context;
+        }
+  
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Show(string search)
+        { 
+            var users = from m in db.Users
+                         select m;
+
+            if (!String.IsNullOrEmpty(search))
+            {
+                users = users.Where(s => s.Name.Contains(search));
+            }
+            {
+                return View(users);
+            }
+        }
+        [Authorize(Roles = "admin")]
+        public IActionResult Edit()
+        {
+            return View(db.Users.ToList());
+        }
+        [HttpPost]
+        public ActionResult AddUser()
+        {
+            string nameString = Request.Form.FirstOrDefault(p => p.Key == "name").Value;
+            string emailString = Request.Form.FirstOrDefault(p => p.Key == "email").Value;
+            string phoneString = Request.Form.FirstOrDefault(p => p.Key == "phone").Value;
+            string BirhdayString = Request.Form.FirstOrDefault(p => p.Key == "birhday").Value;
+
+            DateTime BirhdayS = DateTime.Parse(BirhdayString);
+
+            if (phoneString == null || phoneString == "")
+            {
+                phoneString = "/-/";
+            }
+
+            db.AddRange(
+                    new User
+                    {
+                        Name = nameString,
+
+                        Email = emailString,
+                        Birhday = BirhdayS,
+                        Phone = phoneString
+                    }
+                    );
+            db.SaveChanges();
+            return RedirectToAction("Edit");
+        }
+        public ActionResult EditUser()
+        {
+            string nameString = Request.Form.FirstOrDefault(p => p.Key == "name").Value;
+            string emailString = Request.Form.FirstOrDefault(p => p.Key == "email").Value;
+            string phoneString = Request.Form.FirstOrDefault(p => p.Key == "phone").Value;
+            string BirhdayString = Request.Form.FirstOrDefault(p => p.Key == "birhday").Value;
+
+            string ID = Request.Form.FirstOrDefault(p => p.Key == "id").Value;
+            DateTime BirhdayS = DateTime.Parse(BirhdayString);
+            int num = Convert.ToInt32(ID);
+
+            User q = db.Users.Where(o => o.Id == num).FirstOrDefault();
+
+            q.Name = nameString;
+            q.Email = emailString;
+            q.Phone = phoneString;
+            q.Birhday = BirhdayS;
+
+            db.SaveChanges();
+            return RedirectToAction("Edit");
+        }
+        public ActionResult DeleteUser()
+        {
+            string ID = Request.Form.FirstOrDefault(p => p.Key == "id").Value;
+            int num = Convert.ToInt32(ID);
+
+            User q = db.Users.Where(o => o.Id == num).FirstOrDefault();
+
+            db.Users.Remove(q);
+             db.SaveChanges();
+
+            return RedirectToAction("Edit");
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Adresbook.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        [Required (ErrorMessage = "Не указано имя")]
+        [RegularExpression(@"[A-Za-z] ", ErrorMessage ="Неверный формат имени")]
+        public string Name { get; set; }
+        [Required (ErrorMessage = "Не указан номер телефона")]
+        public string Phone { get; set; }
+        [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}", ErrorMessage = "Не указан электронный адрес")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Не указана дата рождения")]
+        public DateTime Birhday { get; set; }
+    }
+}
